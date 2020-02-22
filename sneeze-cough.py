ba200a28373a1496815c5905090f1d9538b12fc5
edab <ing.dabramo@gmail.com>
2020-02-22 13:03:01 +0100

    feat: add pose model layer to the image

--- sneeze-cough.py
+++ sneeze-cough.py
@@ -6,7 +6,6 @@ import numpy as np
 
 from openvino.inference_engine import IENetwork, IECore
 
-
 # The dataset can be downloaded from here:
 # https://web.bii.a-star.edu.sg/archive/machine_learning/Projects/FluRecognition/videos/biisc.zip
 
@@ -43,36 +42,71 @@ def handle_output(model_type):
     '''
     if model_type == "POSE":
         return handle_pose
-    # elif model_type == "TEXT":
-    #     return handle_text
-    # elif model_type == "CAR_META":
-    #     return handle_car
+    elif model_type == "ACTION":
+        return handle_action
     else:
         return None
 
-
-def handle_pose(output, input_shape):
+def handle_pose(output, frame, args):
     '''
     Handles the output of the Pose Estimation model.
     Returns ONLY the keypoint heatmaps, and not the Part Affinity Fields.
     '''
-    heatmaps = output['Mconv7_stage2_L2']
-    #print("Heatmaps shape: {}".format(heatmaps.shape))
-    assert(len(BODY_PARTS) <= heatmaps.shape[1])
-    # TODO 2: Resize the heatmap back to the size of the input
-    #print(heatmaps.shape[1]) ===> 19
-    #print(input_shape[0]) ===> 750
-    #print(input_shape[1]) ===> 1000
-    #print(len(heatmaps[0])) ===> 19, heatmaps[1] ===> index 1 is out of bounds for axis 0 with size 1
-    out_heatmap = np.zeros([heatmaps.shape[1], input_shape[0], input_shape[1]])
-    for h in range(len(heatmaps[0])):
-        out_heatmap[h] = cv2.resize(heatmaps[0][h], input_shape[0:2][::-1])
 
-    return out_heatmap
+    # Get only heatmaps from model output
+    heatmaps = output['Mconv7_stage2_L2']
+
+    # Check if the output are correct
+    assert(len(BODY_PARTS) <= heatmaps.shape[1])
+
+    # Init structures
+    points = []
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    for i in range(len(BODY_PARTS)):
+
+        # Slice heatmap of corresponding body's partRWrist.
+        heatMap = heatmaps[0, i, :, :]
+
+        # TODO: keep track of multiple poses
+        # Originally, we try to find all the local maximums. To simplify a sample
+        # we just find a global one. However only a single pose at the same time
+        # could be detected this way.
+        _, conf, _, point = cv2.minMaxLoc(heatMap)
+        x = (frameWidth * point[0]) / heatmaps.shape[3]
+        y = (frameHeight * point[1]) / heatmaps.shape[2]
+
+        # Add a point if it's confidence is higher than threshold.
+        points.append((int(x), int(y)) if conf > args.t else None)
+
+    for pair in POSE_PAIRS:
+        partFrom = pair[0]
+        partTo = pair[1]
+        assert(partFrom in BODY_PARTS)
+        assert(partTo in BODY_PARTS)
+
+        idFrom = BODY_PARTS[partFrom]
+        idTo = BODY_PARTS[partTo]
+
+        if points[idFrom] and points[idTo]:
+            cv2.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
+            cv2.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
+            cv2.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv2.FILLED)
+
+    # TODO: Generate an event when LWrist or RWrist come close to Nose
+
+    return frame
+
+def handle_action(output, input_shape, args):
+
+    # TODO: Implement the inference for the ACTION_RECOGNITION_MODEL
+    print("Error: not yet implemented")
+    quit()
 
 def preprocessing(frame, H, W):
     '''
-    Given an input image, height and width:
+    Given an input image, height and width:1
     - Resize to width and height
     - Transpose the final "channel" dimension to be first
     - Reshape the image to add a "batch" of 1 at the start
@@ -86,43 +120,33 @@ def preprocessing(frame, H, W):
 
 def create_output_image(model_type, image, output):
     '''
-    Using the model type, input image, and processed output,
+    Using the model type, input # TODO: Implement the inference for the ACTION_RECOGNITION_MODEL
+    print("Error: not yet implemented")
+    quit()image, and processed output,
     creates an output image showing the result of inference.
     '''
-    #print('-' * 80)
-    #print('[3.Create output image]')
 
     if model_type == "POSE":
 
-        # Combine with original image
-        #print('      image: {}'.format(image.shape))
-        #print('    output1: {}'.format(output.shape))
-
         # Remove final part of output not used for heatmaps
         output = output[:-1]
-        #print('    output2: {}'.format(output.shape))
 
         # Get only pose detections above 0.5 confidence, set to 255
         for c in range(len(output)):
             output[c] = np.where(output[c]>0.5, int(255), int(0))
-        #print('    output3: {}'.format(output.shape))
 
         # Sum along the "class" axis
         output = np.sum(output, axis=0)
 
-        #print('    output4: {}'.format(output.shape))
-
         # Get semantic mask
-        pose_mask = get_mask(output).astype(int)
+        pose_mask = get_mask(output)
 
         # Combine with original image
-        output_image = image.astype(np.single) + pose_mask.astype(np.single)
-        #print(image[1][1])
-        #rint(pose_mask[1][1])
-        #print(output_image[1][1])
-        #cv2.addWeighted(image, 0.7, pose_mask, 0.3, 0)
+        output_image = image + pose_mask
 
-        return output_image
+        #output_image = cv2.addWeighted(image, 0.7, pose_mask, 0.3, 0)
+
+        return pose_mask
 
 def get_args():
     '''
@@ -134,11 +158,14 @@ def get_args():
     i_desc = "The location of the input file (default: 'dataset/biisc/videos/S003_M_COUG_WLK_FCE.avi')"
     d_desc = "Target device: CPU, GPU, FPGA, MYRIAD, MULTI:CPU,GPU, HETERO:FPGA,CPU (default: 'CPU')"
     m_desc = "The model to use ['POSE' or 'ACTION'] (default: 'POSE')"
+    t_desc = "Threshold value for pose parts heat map (default: 0.5)"
 
     # Create the arguments
-    parser.add_argument("-i", help=i_desc, default=0)
+    parser.add_argument("-i", help=i_desc, default=INPUT_STREAM)
     parser.add_argument("-d", help=d_desc, default='CPU')
     parser.add_argument("-m", help=m_desc, default='POSE')
+    parser.add_argument('-t', help=t_desc, default=0.5, type=float)
+
     args = parser.parse_args()
 
     return args
@@ -152,7 +179,7 @@ def load_network(args):
         # Initialise the class
         Network = IENetwork(model=HUMAN_POSE_MODEL, weights=HUMAN_POSE_WEIGHTS)
 
-        # Get Input Layer Information
+        # Get Input Layer Informationoutput_image
         PoseEstimationInputLayer = next(iter(Network.inputs))
         print("Pose Estimation Input Layer: ", PoseEstimationInputLayer)
         print(Network.inputs)
@@ -176,6 +203,10 @@ def load_network(args):
 
     elif args.m == 'ACTION':
 
+        # TODO: Implement the the ACTION_RECOGNITION_MODEL loading
+        print("Error: not yet implemented")
+        quit()
+
         Network = IENetwork(model=ACTION_RECOGNITION_MODEL)
 
         return Network
@@ -189,8 +220,10 @@ def get_mask(processed_output):
     Given an input image size and processed output for a semantic mask,
     returns a masks able to be combined with the original image.
     '''
+# START_TEST
     # Create an empty array for other color channels of mask
     empty = np.zeros(processed_output.shape)
+
     # Stack to make a Green mask where text detected
     mask = np.dstack((empty, processed_output, empty))
 
@@ -224,52 +257,15 @@ def infer_on_video(args, N, C, H, W, Network, ExecutableNetwork, cap):
         fps = 1. / inf_time
 
         # Write Information on Image
-        text = 'Pose Estimation - FPS: {}, INF: {}'.format(round(fps, 2), round(inf_time, 4))
-        cv2.putText(frame, text, (0, 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (0, 125, 255), 1)
+        text = 'Sneeze-Cough estimation - FPS: {:.1f}, INF: {:.2f}'.format(round(fps, 2), round(inf_time, 4))
+        cv2.putText(frame, text, (0, 20), cv2.FONT_HERSHEY_COMPLEX, 0.4, (0, 125, 255), 1)
 
+        # Add model layer
         process_func = handle_output(args.m)
-        processed_output = process_func(results, frame.shape)
+        points = process_func(results, frame, args)
 
-        # Create an output image based on network
-        #try:
-        output_image = create_output_image(args.m, frame, processed_output)
-
-        #except:
-        #    output_image = frame
-        #    print("Error")
-
-        # https://github.com/opencv/opencv/blob/master/samples/dnn/openpose.py
-        # points = []
-        # for i in range(len(BODY_PARTS)):
-        #     # Slice heatmap of corresponding body's part.
-        #     heatMap = out[0, i, :, :]
-        #
-        #     # Originally, we try to find all the local maximums. To simplify a sample
-        #     # we just find a global one. However only a single pose at the same time
-        #     # could be detected this way.
-        #     _, conf, _, point = cv.minMaxLoc(heatMap)
-        #     x = (frameWidth * point[0]) / out.shape[3]
-        #     y = (frameHeight * point[1]) / out.shape[2]
-        #
-        #     # Add a point if it's confidence is higher than threshold.
-        #     points.append((int(x), int(y)) if conf > args.thr else None)
-        # for pair in POSE_PAIRS:
-        #         partFrom = pair[0]
-        #         partTo = pair[1]
-        #         assert(partFrom in BODY_PARTS)
-        #         assert(partTo in BODY_PARTS)
-        #
-        #         idFrom = BODY_PARTS[partFrom]
-        #         idTo = BODY_PARTS[partTo]
-        #
-        #         if points[idFrom] and points[idTo]:
-        #             cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
-        #             cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-        #             cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-        #
-        #     t, _ = net.getPerfProfile()
-
-        cv2.imshow('Window', output_image)
+        # Show computed image
+        cv2.imshow('Window', frame)
 
         # Break if escape key pressed
         if key_pressed == 27:
